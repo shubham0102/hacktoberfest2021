9ad119c7ffb734abee97a98b9b1b05a1ad3d5c18
Shubham Gupta <shubhamgupta7440@gmail.com>
2022-10-16 11:24:34 +0530

    feat: added calbback example

---
+++ callback.js
@@ -0,0 +1,14 @@
+function myDisplayer(some) {
+  console.log(some)
+}
+
+function myFirst() {
+  myDisplayer("Hello");
+}
+
+function mySecond() {
+  myDisplayer("Goodbye");
+}
+
+myFirst();
+mySecond();
